3ec3bbef4f408f87851811ceb70b96f0c69ebbac
samuel-oliveira-rodrigues-da-silva <75077588+samuel-oliveira-rodrigues-da-silva@users.noreply.github.com>
2021-06-18 07:10:26 -0300

    Tirei a resposta

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function numb (){
 var num = parseInt(Math.random()*98+11)
 var raiz = num*num
-var resp = window.prompt(`Qual é a raíz quadrada de ${raiz}? que é ${num}`)
+var resp = window.prompt(`Qual é a raíz quadrada de ${raiz}?`)
 if(resp == num){
     alert(`Você acertou, era ${num}`)
 }
